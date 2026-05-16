13bb63c8b1690411e827a6bcb7f3908cbdf99ab1
Malte Lunkeit <malte.lunkeit@gmail.com>
2026-05-16 21:06:57 +0200

    fix: set fallback route to redirectToKeycloak

--- routes/auth.php
+++ routes/auth.php
@@ -30,4 +30,4 @@ Route::post('/logout', [Auth\LoginController::class, 'logout'])
     ->name('auth.logout');
 
 // Catch any other combinations of routes and pass them off to keycloak.
-Route::fallback([Auth\KeycloakController::class, 'index']);
+Route::fallback([Auth\KeycloakController::class, 'redirectToKeycloak']);
